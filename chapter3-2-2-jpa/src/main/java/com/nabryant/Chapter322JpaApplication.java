f48010a80b462fafb4af9cc24f8f33784e90bab9
NaWang <nawang005194.sohu-inc.com>
2018-03-11 21:18:06 +0800

    add jpa

---
+++ chapter3-2-2-jpa/src/main/java/com/nabryant/Chapter322JpaApplication.java
@@ -0,0 +1,12 @@
+package com.nabryant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter322JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter322JpaApplication.class, args);
+	}
+}
--- chapter3-2-7/src/test/java/com/example/chapter327/Chapter327ApplicationTests.java
+++ chapter3-2-7/src/test/java/com/example/chapter327/Chapter327ApplicationTests.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class Chapter327ApplicationTests {
 
 	@Autowired
